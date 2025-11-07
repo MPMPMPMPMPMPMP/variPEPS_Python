f5b56804f28ac92aa148c64e169a96e6558b7254
Marc-Philipp Tillschneider <m-p.tillschneider@tum.de>
2025-11-07 13:38:35 +0100

    changed a symbol in logger

--- varipeps/optimization/optimizer.py
+++ varipeps/optimization/optimizer.py
@@ -1061,7 +1061,7 @@ def optimize_peps_network(
 
         step_str = f"{float(linesearch_step):0.3f}" if linesearch_step is not None else "None"
         logger.info(
-            "📉 %4d | E=%.8f ΔE=%.2e | r=%d | |∇ψ|=%.2e | ls=%s | ε_tr=%.1e | χ=%d | t=%.0fs",
+            "📉 %4d | E=%.8f ΔE=%.2e | r=%d | ‖∇ψ‖=%.2e | ls=%s | ε_tr=%.1e | χ=%d | t=%.0fs",
             int(count),
             float(working_value),
             float(step_energies[random_noise_retries][-2] - working_value) if len(step_energies[random_noise_retries]) > 1 else 0.0,
