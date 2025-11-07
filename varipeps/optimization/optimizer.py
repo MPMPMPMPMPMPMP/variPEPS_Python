e0c0c8b50285946712b63722040a05dfce878204
Marc-Philipp Tillschneider <m-p.tillschneider@tum.de>
2025-11-07 11:23:24 +0100

    shortend step message and changed some levels

--- varipeps/optimization/optimizer.py
+++ varipeps/optimization/optimizer.py
@@ -1060,7 +1060,7 @@ def optimize_peps_network(
 
         step_str = f"{float(linesearch_step):0.3f}" if linesearch_step is not None else "None"
         logger.info(
-            "📉 Step %d | Energy: %.8f Δ: %.2e | Retries: %d | Conv: %.3e | Line search step: %s | Max. trunc. err.: %.1e χ: %d | Runtime: %.0f sec",
+            "📉 %4d | E=%.8f ΔE=%.2e | r=%d | |∇ψ|=%.2e | ls=%s | ε_tr=%.1e | χ=%d | t=%.0fs",
             int(count),
             float(working_value),
             float(step_energies[random_noise_retries][-2] - working_value) if len(step_energies[random_noise_retries]) > 1 else 0.0,
